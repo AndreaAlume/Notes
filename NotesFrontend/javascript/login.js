21b920dcee5f00727019f12c8b5cf383750d158f
Andrea Alume <andreaalume66@gmail.com>
2025-12-18 17:33:13 +0100

    Punto di partenza per React project whit Vanilla

--- NotesFrontend/javascript/login.js
+++ NotesFrontend/javascript/login.js
@@ -1,6 +1,3 @@
-import validator from '../../node_modules/emailvalid/index';
-import PasswordValidator from '../../node_modules/password-validator/src/index';
-
 async function renderLogin() {
   const response = await fetch('../templates/login.hbs');
   const source = await response.text();
@@ -8,29 +5,6 @@ async function renderLogin() {
 
   document.querySelector('.main').innerHTML = template({});
 
-  const emailInput = document.getElementById("email-input");
-  const passwordInput = document.getElementById("password-input");
-  const login = document.getElementById("login");
-
-  const schema = new PasswordValidator();
-  schema
-    .is().min(8)
-    .is().max(20)
-    .has().uppercase()
-    .has().lowercase()
-    .has().digits()
-    .has().not().spaces();
-
-  login.addEventListener("click", function() {
-    const emailValid = validator.validate(emailInput.value.trim());
-    const passwordValid = schema.validate(passwordInput.value.trim());
-
-    if (emailValid && passwordValid) {
-      alert("Login valido!");
-    } else {
-      alert("Email o password non valida.");
-    }
-  });
 }
 
 renderLogin();
